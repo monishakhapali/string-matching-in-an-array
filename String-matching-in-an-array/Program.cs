2dd80b160e09930b0c9c5f03eea9bb2490e5c44c
Monisha Khapali <monisha.khapali@logicsolutions.com>
2020-11-15 16:51:35 -0500

    added files

---
+++ String-matching-in-an-array/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace String_matching_in_an_array
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Hello World!");
+            string[] words = { "mass", "as", "hero", "superhero" };
+            IList<string> s =StringMatching(words);
+            Console.WriteLine(string.Join(" ", s));
+            Console.ReadLine();
+    }
+        public static IList<string> StringMatching(string[] words)
+        {
+
+            List<string> substring = new List<string>();
+            for(int i=0;i< words.Length;i++)
+            {
+                for(int j=0;j< words.Length;j++)
+                {
+                    if(words[i]!= words[j] && words[j].Contains(words[i]))
+                    {
+                        if(!substring.Contains(words[i]))
+                        {
+                            substring.Add(words[i]);
+                        }
+                    }
+                }
+            }
+            return substring;
+        }
+    }
+}
